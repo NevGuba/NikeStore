6715cdda1a9c614e6eab19c11983b39b4542f84e
NevGuba <135975324+NevGuba@users.noreply.github.com>
2023-09-05 12:23:13 +0100

    Add files via upload

---
+++ src/Sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import CustomerReviews from "./CustomerReviews";
+import SpecialOffer from "./SpecialOffer";
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Footer,
+    Services,
+    Subscribe,
+    CustomerReviews,
+    SpecialOffer
+}
